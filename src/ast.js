968fd4db5bf264d76e829c81576ef6274a7fb6d4
Javier Fernandes <javier.fernandes@gmail.com>
2017-08-05 18:42:27 -0300

    Styling and adding ability to enable/disable features on the AST like filters

--- src/ast.js
+++ src/ast.js
@@ -14,18 +14,36 @@ const label = (kind, text) => span(img.node[kind](), span(text))()
 const node = (kind, text) => tree({
     key: nodeKey++,
     defaultCollapsed: false,
-    nodeLabel: label(kind, text)
+    nodeLabel: label(kind, text),
+    itemClassName: kind
 })
 
-const toTree = ast => div.ast(
+const toTree = (ast, features) => div.ast(
     ast && node('object', ast.type)(
-        keys(ast).map(key => {
+        keys(ast)
+        .filter(key => shouldShow(features, key, ast[key]))
+        .map(key => {
             if (key === 'type') return []
-            if (ast[key] instanceof Array) return ast[key].length ? node('array', key)(ast[key].map(toTree)) : div(label('array', key))
-            if (typeof ast[key] === 'object' && ast[key].type) return node('object', key)(toTree(ast[key]))
-            return div(label('attribute', key + ' = ' + ast[key]))
+            if (ast[key] instanceof Array) return ast[key].length ? node('array', key)(ast[key].map(_ => toTree(_, features))) : div(label('array', key))
+            if (typeof ast[key] === 'object' && ast[key].type) return node('object', key)(toTree(ast[key], features))
+            if (typeof ast[key] === 'function') return div(label('attribute', functionToLabel(ast[key])))
+            return div(label('attribute', `${key} = ${toValue(ast[key])}`))
         })
     )
 )
+
+const shouldShow = (features, key, value) => {
+  if (typeof value === 'function' && features) { return features.method }
+  if (key === 'parent' && features) { return features.parent }
+  if (key === 'scope' && features) { return features.scope }
+  return true
+}
+
+const FN_EXTRACTOR_REGEXP = /function ([^(]*)[ ]*\((.*)\).*/
+const functionToLabel = fn => {
+  const [, name, args] = fn.toString().split(FN_EXTRACTOR_REGEXP)
+  return `${name}(${args})`
+} 
+const toValue = v => (typeof v === 'function') ? `${v.name}()` : v
 
 export default toTree
